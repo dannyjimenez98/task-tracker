04ee3ba0df947c760a405a898542c699fc4bf058
Danny Jimenez <dannyajimenez98@gmail.com>
2026-09-20 16:25:14 -0400

    feat: implement 'add' task subcommand
    'add' subcommand takes task description and appends new task to the tasklist slice from json file
    
    task ID is auto-incremented from length of tasklist slice

--- internal/cli/cli.go
+++ internal/cli/cli.go
@@ -2,16 +2,45 @@
 package cli
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
 	"os"
 	"strconv"
+	"time"
+
+	"github.com/dannyjimenez98/task-tracker/internal/task"
 )
+
+// Add appends a new task to the slice of tasks and writes it to the tasklist.json file.
+func add(taskDescription string) {
+
+	tasklist := task.GetTaskList()
+
+	tasks := append(tasklist, task.Task{
+		ID: len(tasklist) + 1,
+		Description: taskDescription,
+		Status: "todo",
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+	})
+
+	tasklistJSON, err := json.MarshalIndent(tasks, "", "  ") 
+	if err != nil {
+		fmt.Println("marshal error: ", err)
+	}
+
+	if err := os.WriteFile("tasklist.json", tasklistJSON, 0644); err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println("Successfully added task.")
+}
 
 // Start parses the command line arguments and calls the appropriate function
 // to handle the requested action.
 func Start() {
-
+	// Subcommands
 	addCmd := flag.NewFlagSet("add", flag.ExitOnError)
 	deleteCmd := flag.NewFlagSet("delete", flag.ExitOnError)
 	updateCmd := flag.NewFlagSet("update", flag.ExitOnError)
@@ -20,16 +49,17 @@ func Start() {
 	// "add" expects 1 argument: taskDescription (string)
 	case "add": 
 		if err := addCmd.Parse(os.Args[2:]); err != nil {
-			fmt.Printf("error: %s\n", err)
+			fmt.Printf("parsing error: %s\n", err)
 			return
 		}
 		taskDescription := os.Args[2]
 		fmt.Printf("adding task %s\n", taskDescription)
-
+		add(taskDescription)
+	
 	// "delete" expects 1 argument: taskID (int)
 	case "delete": 
 		if err := deleteCmd.Parse(os.Args[2:]); err != nil {
-			fmt.Printf("error: %s\n", err)
+			fmt.Printf("parsing error: %s\n", err)
 			return
 		}
 		taskID, err := strconv.Atoi(os.Args[2])
@@ -41,7 +71,7 @@ func Start() {
 	// "update" expects 2 arguments: taskID (int), newTaskDescription (string)
 	case "update": 
 		if err := updateCmd.Parse(os.Args[2:]); err != nil {
-			fmt.Printf("error: %s\n", err)
+			fmt.Printf("parsing error: %s\n", err)
 			return
 		}
 		taskID, err := strconv.Atoi(os.Args[2])
@@ -55,3 +85,4 @@ func Start() {
 		os.Exit(1)
 	} 
 }
+
